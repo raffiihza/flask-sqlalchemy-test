4976af205ac5340cefe4a53283681b6cddf90dbf
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2024-06-03 15:01:36 +0000

    feat: two apps

--- crud/app.py
+++ crud/app.py
@@ -1,6 +1,25 @@
-from flask import render_template, request, redirect, url_for, flash
-from models import Informasi, db
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from datetime import datetime
 
+# Initialization
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+# Models
+class Informasi(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    judul = db.Column(db.String(100), nullable=False)
+    isi = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+# Migrations
+with app.app_context():
+    db.create_all()
+
+# Controllers
 def index():
     informasis = Informasi.query.all()
     return render_template('index.html', informasis=informasis)
@@ -44,3 +63,16 @@ def delete(informasi_id):
         return redirect(url_for('index'))
     except:
         return 'There was a problem deleting that information'
+
+# Routes
+def register_routes(app: Flask):
+    app.add_url_rule('/', view_func=index, methods=['GET'])
+    app.add_url_rule('/create', view_func=create, methods=['GET', 'POST'])
+    app.add_url_rule('/show/<int:informasi_id>', view_func=show, methods=['GET'])
+    app.add_url_rule('/edit/<int:informasi_id>', view_func=edit, methods=['GET', 'POST'])
+    app.add_url_rule('/delete/<int:informasi_id>', view_func=delete, methods=['POST'])
+
+register_routes(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
